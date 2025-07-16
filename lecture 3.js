c3ed52b74a1cd147bcfd99fba59e748c24f211ba
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-16 14:25:21 +0530

    loop concept

---
+++ lecture 3.js
@@ -0,0 +1,11 @@
+
+
+console.log(1394 % 10);
+console.log(Math.floor(1394/10));
+console.log(139 % 10);
+console.log(Math.floor(139/10));
+console.log(13 % 10);
+console.log(Math.floor(13/10));
+console.log(1 % 10);
+console.log(Math.floor(1/10));
+
