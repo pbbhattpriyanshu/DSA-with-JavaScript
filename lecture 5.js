db3802aff51136db2aacb8d0bbd4e42b3fcd0034
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-26 19:14:46 +0530

    Move zeros

--- lecture 5.js
+++ lecture 5.js
@@ -34,30 +34,98 @@
 
 //Max value in array
 // let arr = [54, 7, 83, 65, 23, 91, 13, 37, 68, 42, 19, 88, 30, 77, 4, 21, 96, 72, 6, 58]
-// let Max = Math.max(arr[0], arr[1]) //54
-// let Max2 = Math.min(arr[0], arr[1]) //7
+// let Max = arr[0]
 
-// for (let i=2; i<arr.length;i++){
+// for (let i=1; i<arr.length;i++){
 //     if (arr[i] > Max) {
-//         Max2 = Max;
 //         Max = arr[i]
-//     } else if (Max2<arr[i] && Max != arr[i]) {
-//         Max2 = arr[i]
 //     }
 // }
 
 // console.log(Max);
-// console.log(Max2);
 
-//New Reverse Array // print reverse order - ❌
 
-let arr = [54, 7, 83, 65, 23, 91, 13, 37, 68, 42, 19, 88, 30, 77, 4, 21, 96, 72, 6, 58]
-let temp = new Array(arr.length)
-let j = 0
-for(let i=arr.length-1; i>=0; i--) {
-    temp[j] = arr[i];
-    j++
-}
+// 2 Max Element
+// let arr = [154,7,83,65,71,91,25, 154]
+// let max1 = Math.max(arr[0], arr[1])
+// let max2 = Math.min(arr[0], arr[1])
 
-console.log(temp);
-console.log(arr);
+// for (let i = 2; i < arr.length; i++) {
+//     if (arr[i] > max1) {
+//         max2 = max1
+//         max1 = arr[i];
+//     } else if (arr[i] > max2 && max1 != arr[i])  {
+//         max2 = arr[i]
+//     }
+// }
+// console.log(max1);
+// console.log(max2);
+
+//Min value in array
+// let arr = [54, 7, 83, 65, 23, 91, 13, 37, 68, 42, 19, 88, 30, 77, 4, 21, 96, 72, 6, 58]
+// let Min = arr[0]
+
+// for (let i=1; i<arr.length;i++){
+//     if (arr[i] < Min) {
+//         Min = arr[i]
+//     }
+// }
+
+// console.log(Min);
+
+// Reverse array
+//O(n)	Manual reversal using a new array
+// let arr = [1, 4, 3, 2, 6, 5];
+// let temp = new Array(arr.length); // creates an empty array of the same length
+// let j = 0;
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     temp[j] = arr[i]; // fill temp from end of arr
+//     j++;
+// }
+
+// arr = temp; // assign reversed array to arr
+
+//Two Pointer	O(1)	In-place reversal using two indices
+
+// let left = 0;
+// let right = arr.length-1
+
+// while (left < right) {
+//     //swap
+//     let temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
+
+//     //Move pointers
+//     left++;
+//     right--;
+// }
+
+// console.log(arr);
+
+// Remove Duplicates from Sorted Array
+
+// let nums = [1,1,2]
+// console.log(nums.length === 0) //false
+// console.log(nums[0] !== nums[1]); //true
+
+//Move zeros
+// let arr = [0,1,0,3,12]
+// let j = 0
+
+// // Move all non-zero elements forward
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//         arr[j] = arr[i];
+//         j++;
+//     }
+// }
+
+// // Fill the rest with zeros
+// while (j < arr.length) {
+//     arr[j] = 0;
+//     j++;
+// }
+// console.log(arr);
+
