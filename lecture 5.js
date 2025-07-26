0c96927d77adc78272c9784addce3dd4bf71f36f
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-26 19:44:32 +0530

    Remove Duplicates from Sorted Array

--- lecture 5.js
+++ lecture 5.js
@@ -105,10 +105,29 @@
 // console.log(arr);
 
 // Remove Duplicates from Sorted Array
+// let removeDuplicate = function(nums) {
+//     if (nums.length === 0) return 0;
 
-// let nums = [1,1,2]
-// console.log(nums.length === 0) //false
-// console.log(nums[0] !== nums[1]); //true
+//     let i = 0; // last unique index
+
+//     for (let j = 1; j < nums.length; j++) {
+//         if (nums[j] !== nums[i]) {
+//             i++;
+//             nums[i] = nums[j]; // overwrite duplicate
+//         }
+//     }
+
+//     return i + 1; // total number of unique elements
+// };
+
+
+// let nums = [1, 1, 2];
+// let k = removeDuplicate(nums);
+
+// console.log("k =", k);                      // Output: 2
+// console.log("Modified array:", nums);      // Output: [1, 2, ...]
+// console.log("Unique values:", nums.slice(0, k)); // Output: [1, 2]
+
 
 //Move zeros
 // let arr = [0,1,0,3,12]
