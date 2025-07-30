9914064c7ccbb6c54caceb69a9fe16277dadd0af
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-07-30 23:23:08 +0530

    Check if array is palindrome

--- lecture 5.js
+++ lecture 5.js
@@ -216,3 +216,36 @@
 // let arr = [1,2,3,4,5]//15
 
 // sumofArray(arr)
+
+//Check if array is palindrome
+function checkPalindrome(arr) {
+  let temp = new Array(arr.length);
+  let j = 0;
+
+  // Reverse the array manually into temp
+  for (let i = arr.length - 1; i >= 0; i--) {
+    temp[j] = arr[i];
+    j++;
+  }
+
+  // Check each element one by one
+  let isPalindrome = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== temp[i]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  // Output result
+  if (isPalindrome) {
+    console.log(`${arr} is a palindrome!!`);
+  } else {
+    console.log(`${arr} is not a palindrome!!`);
+  }
+  console.log("Reversed array:", temp);
+}
+
+
+let arr = [1,2,3]
+checkPalindrome(arr)
